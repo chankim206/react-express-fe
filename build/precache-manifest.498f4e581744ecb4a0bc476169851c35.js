ed7f888aeefd0760ba45816b6f9bcfb6ca6b4def
Young-Chan Kim <chankim206@gmail.com>
2019-12-03 13:39:21 -0800

    testing

---
+++ build/precache-manifest.498f4e581744ecb4a0bc476169851c35.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ce5e6aca62e28d2a074565bcf78f94e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7f4837aacf0184b330af",
+    "url": "/static/css/main.39389d5e.chunk.css"
+  },
+  {
+    "revision": "8319091325216aed3224",
+    "url": "/static/js/2.9662e9eb.chunk.js"
+  },
+  {
+    "revision": "7f4837aacf0184b330af",
+    "url": "/static/js/main.c6512e38.chunk.js"
+  },
+  {
+    "revision": "1337678ae3301a2c8a7b",
+    "url": "/static/js/runtime-main.8eac158a.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
